db989849c12854e6095a50ca673924081777fd3b
frogtd <ash@frogtd.com>
2022-01-25 18:28:44 -0500

    Comment ML.java

--- src/main/java/frc/subsystems/ML.java
+++ src/main/java/frc/subsystems/ML.java
@@ -4,15 +4,12 @@ import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.networktables.NetworkTableInstance;
-import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Robot;
 import frc.utils.BoundingBox;
 
-import java.util.Scanner;
 
-import static java.lang.Double.parseDouble;
 
 public class ML extends SubsystemBase {
     NetworkTable ml,camera;
@@ -28,42 +25,71 @@ public class ML extends SubsystemBase {
 //        camera =  NetworkTableInstance.getDefault().getTable("ML"+ NetworkTable.PATH_SEPARATOR+"MLOut");
 //        res = camera.getEntry("mode");
 //        string = res.getString("320x240 MJPEG 30 fps");
+        // TODO: get these from network tables again
         middleX = 160/2;
         middleY = 120/2;
     }
     public double movement() {
+        // get array of coords
+        // its format is [x1,y1,x2,y2], and repeat this for as many times as possible 
+        // length therefore is always a multiple of 4.
+        double[] inputCoords = getDoubleArray();
 
-        double[] inputCords = getDoubleArray();
-        BoundingBox[] boxes = new BoundingBox[inputCords.length/4];
-        for (int i = 1; i <= inputCords.length/4; i++) {
-            boxes[i-1] = new BoundingBox(inputCords[(i*4)-4], inputCords[(i*4)-3], inputCords[(i*4)-2], inputCords[(i*4)-1]);
+        // because length is a multiple of 4, and divsions round down, this will end up being 1/4 the length of 
+        // the `inputCoords` array
+        BoundingBox[] boxes = new BoundingBox[inputCoords.length/4];
+
+        // loop through boxes starting at zero and going up to the total length of the array 
+        for (int i = 0; i < boxes.length; i++) {
+
+            // i*4 is the index of the first element in a set of coords for example:
+            // [0,1,2,3,4,5,6,7]
+            //  ^ i=0   ^i=1
+            // adding 0,1,2,3 gets the elements of the coords, x1, y1, x2, y2       
+            boxes[i] = new BoundingBox(inputCoords[(i*4)+0], inputCoords[(i*4)+1], inputCoords[(i*4)+2], inputCoords[(i*4)+3]);
         }
+        // find max bounding box by area, therefore being the closet. Theoretically something could be closer if 
+        // it was on the edge but that's annoying to deal with 
+        
+        // min area element (0)
         BoundingBox largest = new BoundingBox(middleX, middleY, middleX, middleY);
         for (BoundingBox x : boxes) {
+            // if new box is larger than the largest weve found so far..
             if (x.area > largest.area) {
+                // assign new one to be the current largest
                 largest = x;
             }
         }
-        return (((largest.x1 +largest.x2)/2) - middleX) / middleX;
+
+        // center of bounding box
+        double center = (largest.x1 +largest.x2)/2;
+
+        // center - middle x will make a value -middlex.. middlex
+        // dividing by middle x will then make that -1..1
+        return ((center) - middleX) / middleX;
     }
 
     public double[] getDoubleArray() {
+        // get boxes, in the form of a string, example:
+        // "[1, 2, 3, 4]""
         String boxesString = boxes.getString("[]");
 
+        // remove starting bracket and ending bracket
+        // then split on ", " creating a list of numbers in the form of a string:
+        // ["1", "2", "3", "4"]
         String[] numsString = boxesString.substring(1,boxesString.length()-2).split(", ");
+
+        // make a double array to hold the new doubles 
         double[] nums = new double[numsString.length];
-        for (int x = 0; x < nums.length; x++) {
+
+        // loop through the strings..
+        for (int x = 0; x < numsString.length; x++) {
+            // and assign the parsed versions to the nums array 
             nums[x] = Double.parseDouble(numsString[x]);
         }
+
+        // the array now has the doubles filled and now can be returned. huzzah!
         return nums;
-//        Scanner scanner = new Scanner(boxesString);
-//        scanner.next("\\[");
-//        double[] array = new double[(int) boxesString.chars().filter(ch -> ch == ',').count() + 1];
-//        for (int x = 0; x < array.length; x++) {
-//            array[x] = scanner.nextDouble();
-//            scanner.next(", ");
-//        }
-//        return array;
 
     }
 
